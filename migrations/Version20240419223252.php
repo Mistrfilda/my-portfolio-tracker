6e9497b3b4072b9cff0af90d62408fdb67276c0d
Filip Kuchar <filda.kuchar@seznam.cz>
2024-04-20 16:05:02 +0200

    add missing migration

---
+++ migrations/Version20240419223252.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240419223252 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE bank_expense ADD main_tag_set_manually TINYINT(1) NOT NULL, ADD other_tag_set_manually TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE bank_expense DROP main_tag_set_manually, DROP other_tag_set_manually');
+    }
+}
